319707938be585a891974fba95de020e9c0a572d
Shon <shonzbso@gmail.com>
2023-03-06 20:01:41 +0500

    Add logger support

--- synopsis/__main__.py
+++ synopsis/__main__.py
@@ -1,7 +1,29 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import sys
+import locale
+from datetime import date
+
+from synopsis.tools import loggerSetup
+
 if __name__ == '__main__':
     """Точка входа в программу.
     """
-    print("hello")
+    # Путь сохранения логов на удалённом сервере
+    logger_path = f'../data/logs/{date.today()}.log'
+    # Инициализация и подключение глобального logger
+    logger = loggerSetup.setup('logger', logger_path)
+
+    logger.info('Program started.')
+
+    # Подгрузка .env файла на windows
+    logger.info(f'Platform is {sys.platform}')
+    if sys.platform == "win32":
+        from dotenv import load_dotenv
+        load_dotenv()
+
+    # Инициализация класса для подключение к базе данных
+    # db = DataBase()
+
+    logger.info(f'Filesystem encoding: {sys.getfilesystemencoding()}, Preferred encoding: {locale.getpreferredencoding()}')
